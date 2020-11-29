46e39f77e4d4ba26b42595a367237a32e86f8df4
felipe santiago monteiro da silva <felipe.santiago@outlook.com>
2020-11-29 19:48:19 +0100

    Adding ShakespeareService and related Integration tests

---
+++ TrueLayer.Services.IntegrationTests/ShakespeareServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrueLayer.Services.IntegrationTests
+{
+    [TestClass]
+    public class ShakespeareServiceTest
+    {
+        private ShakespeareService shakeService;
+        public ShakespeareServiceTest()
+        {
+            shakeService = new ShakespeareService();
+        }
+
+        [TestMethod]
+        public async Task TranslateText_Should_ConvertText()
+        {
+            //Arrange
+            string description = "It deliberately makes itself heavy so it can with­ stand the recoil of the water jets it fires.";
+
+            //Act
+            var result = await shakeService.TranslateText(description);
+
+            //Assert
+            Assert.IsTrue(!string.IsNullOrEmpty(result));
+        }
+
+    }
+}
---
+++ TrueLayer.Services/ShakespeareService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace TrueLayer.Services
+{
+    /// <summary>
+    /// This Service class is responsible for making calls to the Translator
+    /// </summary>
+    public class ShakespeareService
+    {
+        private HttpClient httpClient;
+        public ShakespeareService()
+        {
+            this.httpClient = new HttpClient()
+            {
+                BaseAddress = new System.Uri(
+                    "https://api.funtranslations.com/")
+            };
+        }
+
+        /// <summary>
+        /// Post an text to the shakespeare api and receives the converted
+        /// desccription. Currently not using API Keys since it is the free
+        /// version.
+        /// </summary>
+        /// <param name="toBeTranslated"></param>
+        /// <returns>Converted Shakespeare description or null</returns>
+        public async Task<string> TranslateText(string toBeTranslated)
+        {
+            string result = null;
+            string text = HttpUtility.UrlEncode(toBeTranslated);
+            var response = await httpClient.GetAsync($"translate/" +
+                $"shakespeare.json?text=" + $"{text}");
+            if (response.IsSuccessStatusCode) {
+
+                dynamic contentResult = JsonConvert.DeserializeObject(
+                    await response.Content.ReadAsStringAsync());
+                result = contentResult["contents"]["translated"];
+            }
+            return result;
+        }
+    }
+}
